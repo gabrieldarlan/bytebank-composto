927ce82f4a68c5e9f513c2f840d69661b2586ebb
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-01 02:21:22 -0300

    projeto completo

---
+++ bytebank-composto/src/Conta.java
@@ -0,0 +1,35 @@
+
+public class Conta {
+
+	private double saldo;
+	int agencia;
+	int numero;
+	Cliente titular;
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+	
+	
+}
